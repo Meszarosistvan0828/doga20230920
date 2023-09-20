e2fe53c65003139f2d406fde66d0108d9fbc530c
Meszarosistvan0828 <84836386+Meszarosistvan0828@users.noreply.github.com>
2023-09-20 10:11:49 +0200

    sok js és html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,38 @@
-function elsotulaj()
+var tomb = []
+
+function Tárol()
 {
-    document.getElementById("")
+    var tipus = ""
+    if(document.getElementById("valaszt").value = 1)
+    {
+        tipus = "dízel"
+    }
+    if(document.getElementById("valaszt").value = 2)
+    {
+        tipus = "benzin"
+    }  
+    if(document.getElementById("valaszt").value = 3)
+    {
+        tipus = "elektromos"
+    }
+    
+    var haromajtos = 0
+    var otajtos = 0
+    if(documen.getElementById("3ajto").checked)t
+    {
+        haromajtos++
+    }
+    if(document.getElementById("5ajto").checked)
+    {
+        otajtos++
+    }
+    
+    var elsotulaj = 0
+    if(documen.getElementById("csecs").checked)t
+    {
+        haromajtos++
+    }
+    
+
+
 }
